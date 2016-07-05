60fc7fc12e2bda787970931242759b2b98c4cc38
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-05 18:25:23 +0300

    Added some tests

--- NewtonMethod.UnitTests/NewtonUnitTests.cs
+++ NewtonMethod.UnitTests/NewtonUnitTests.cs
@@ -7,12 +7,38 @@ namespace NewtonMethod.UnitTests
     public class NewtonUnitTests
     {
         [TestMethod]
-        public void Root_27powerMinus3_Expected3()
+        public void Root_27powerMinus2_Expected3()
         {
             double result = Newton.Root(9, 2);
-            Assert.IsTrue(result - 3 < 0.0001);
+            Assert.IsTrue(Math.Abs(result - 3) < 0.0001);
         }
 
-        public void Root_
+        [TestMethod]
+        public void Root_100powerMinus5_Expected2AndAHalf()
+        {
+            double result = Newton.Root(100, 5);
+            Assert.IsTrue(Math.Abs(result - 2.511886) < 0.0001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Root_NotPositivePower_ArgumentExaptionExpected()
+        {
+            Newton.Root(10, -10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Root_NotPositiveAccuracy_ArgumentExaptionExpected()
+        {
+            Newton.Root(10, 3, -5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Root_AccuracyGrateThen1_ArgumentExaptionExpected()
+        {
+            Newton.Root(10, 3, 3);
+        }
     }
 }
